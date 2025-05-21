e3b9a7a390c3064638e090dd1294e9a39a2ca395
mintcat0126 <140900282+mintcat0126@users.noreply.github.com>
2025-05-21 22:15:38 +0900

    if문 연습 프로젝트 생성

--- Cpp Practice/if_practice.cpp
+++ Cpp Practice/if_practice.cpp
@@ -1,1 +1,39 @@
-int main
+#include <stdio.h>
+#include "Turboc.h"
+
+int main()
+{
+	int i,j;
+	
+	printf("정수를 입력하세요 :");
+	scanf_s("%d", &i);
+	if (i%2 == 0)
+	{
+		printf("%d는 짝수입니다.", i);
+	}
+	else
+	{
+		printf("%d는 홀수입니다.", i);
+	}
+
+	delay(3000);
+	clrscr();
+
+	printf("첫 번째 정수를 입력하세요 :");
+	scanf_s("%d", &i);
+	printf("두 번째 정수를 입력하세요 :");
+	scanf_s("%d", &j);
+
+	if (i > j)
+	{
+		printf("%d가 %d보다 큽니다.\n", i, j);
+	}
+	else if (j > i)
+	{
+		printf("%d가 %d보다 작습니다.\n",i, j);
+	}
+	else
+	{
+		printf("%d와 %d는 같은 수입니다.\n",i, j);
+	}
+}
