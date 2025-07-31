c0a31fedbf34e0b569ed7a3352868a34465636ef
mincat0126 <tearswood@gmail.com>
2025-07-31 10:04:49 +0900

    프로젝트 위치 옮기기

---
+++ Projects/CardMemory/main.cpp
@@ -0,0 +1,16 @@
+#include "Turboc.h"
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define ESC 27
+
+enum Status { HIDDEN, FLIP, TEMPFLP };
+
+int nx, ny;
+
+void main()
+{
+
+}
