c789a73a38a903f5f30092615c11f3de8c548c62
mincat0126 <tearswood@gmail.com>
2025-08-11 11:13:37 +0900

    기본셋팅

--- Projects/snake/main.cpp
+++ Projects/snake/main.cpp
@@ -8,7 +8,31 @@
 스테이지 선택
 게임 정지*/
 
+int life;
+int speed;
+int stage;
+
+void InitStage(int stage);
+
 void main()
 {
+	int number;
+	int ch;
+	int x, y;
 
+	life = 5;
+	randomize();
+	setcursortype(NOCURSOR);
+
+	for (;;)
+	{
+		InitStage(stage);
+	}
+}
+
+void InitStage(int stage)
+{
+	int i;
+
+	clrscr();
 }
